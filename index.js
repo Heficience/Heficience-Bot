1410227f0c8c9cb3d072336af25f8a10c45cb6dd
Paullux <paulwoisard@gmail.com>
2021-12-04 22:12:56 +0100

    Add Presentation Utilisateurs

--- index.js
+++ index.js
@@ -548,9 +548,8 @@ client.on('messageReactionAdd', async (reaction_orig, user) => {
   if (message.channel.name === '⌨-langage-connu') {
      addRole(reaction_orig, reaction_orig.message, user);
   }
-  if (message.channel.name === '✔-présentation' && user.id !== client.user.id && (user.roles.find(role => role.id === '904780827123134524') || user.roles.find(role => role.id === '905386175949340672'))) {
+  if (message.channel.name === '✔-présentation' && user.id !== client.user.id && (user.roles.has('904780827123134524') || user.roles.has('905386175949340672'))) {
      const roles = member.roles.cache.array();
-
      let role = message.guild.roles.cache.find(r => r.id === '916776104851820605');
      message.member.addRole(role);
   }
